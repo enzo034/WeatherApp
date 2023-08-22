26be834e945bcce4231891781dabd64176fc0145
Enzo Petela <enzo03453@gmail.com>
2023-08-22 20:02:23 -0300

    Corrections on the script

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const apiKey = "bfd8982d9bd4680fcb4ad78530593ded";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
@@ -7,43 +6,49 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
+
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    if (response.status == 404) {
+    if (response.status == 404) 
+    {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
-    } else {
+    } 
+    else 
+    {
         var data = await response.json();
 
         console.log(data);
 
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
-        if (data.weather[0].main == "Clouds") {
-            weatherIcon.src = "img/clouds.png"
-        }
-        else if (data.weather[0].main == "Clear") {
-            weatherIcon.src = "img/clear.png"
-        }
-        else if (data.weather[0].main == "Rain") {
-            weatherIcon.src = "img/rain.png"
-        }
-        else if (data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "img/drizzle.png"
-        }
-        else if (data.weather[0].main == "Mist") {
-            weatherIcon.src = "img/mist.png"
-        }
+        changeWeatherData(data);
+        changeWeatherImage(data);
 
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
-
     }
 }
 
+//To get the data of the weather from the Json
+function changeWeatherData(data)
+{
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+//To change the icon depending on the weather
+function changeWeatherImage(data)
+{
+    if (data.weather[0].main == "Clouds") weatherIcon.src = "img/clouds.png";
+    else if (data.weather[0].main == "Clear") weatherIcon.src = "img/clear.png";
+    else if (data.weather[0].main == "Rain")  weatherIcon.src = "img/rain.png";
+    else if (data.weather[0].main == "Drizzle") weatherIcon.src = "img/drizzle.png";
+    else if (data.weather[0].main == "Mist") weatherIcon.src = "img/mist.png";
+}
+
+
+//To search with the Enter key
 searchBox.addEventListener("keypress", function(e)
 {
     if(e.key == "Enter")
@@ -53,6 +58,7 @@ searchBox.addEventListener("keypress", function(e)
     }
 })
 
+//To search with a click in the icon
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 });
