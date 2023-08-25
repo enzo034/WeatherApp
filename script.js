1477e05937ded33539a84d20e414e358e4301be3
Enzo Petela <enzo03453@gmail.com>
2023-08-24 21:23:34 -0300

    Added a clock that tells you the time depending on the country that you search.

--- script.js
+++ script.js
@@ -5,18 +5,20 @@ const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
+let intervalId;
+
 async function checkWeather(city) {
 
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    if (response.status == 404) 
-    {
+    if (response.status == 404) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
-    } 
-    else 
-    {
+    }
+    else {
+        
         var data = await response.json();
+
 
         console.log(data);
 
@@ -25,12 +27,27 @@ async function checkWeather(city) {
 
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
+
+        outputActualTime(data);
     }
 }
 
+function outputActualTime(data) {
+    const output = document.querySelector(".card output");
+    
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
+
+    intervalId = setInterval(() => {
+        const inputCityTime = luxon.DateTime.now().toUTC(data.timezone / 60);
+        output.innerHTML = inputCityTime.toFormat("HH:mm:ss");
+
+    }, 1000);
+}
+
 //To get the data of the weather from the Json
-function changeWeatherData(data)
-{
+function changeWeatherData(data) {
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
@@ -38,21 +55,18 @@ function changeWeatherData(data)
 }
 
 //To change the icon depending on the weather
-function changeWeatherImage(data)
-{
+function changeWeatherImage(data) {
     if (data.weather[0].main == "Clouds") weatherIcon.src = "img/clouds.png";
     else if (data.weather[0].main == "Clear") weatherIcon.src = "img/clear.png";
-    else if (data.weather[0].main == "Rain")  weatherIcon.src = "img/rain.png";
+    else if (data.weather[0].main == "Rain") weatherIcon.src = "img/rain.png";
     else if (data.weather[0].main == "Drizzle") weatherIcon.src = "img/drizzle.png";
     else if (data.weather[0].main == "Mist") weatherIcon.src = "img/mist.png";
 }
 
 
 //To search with the Enter key
-searchBox.addEventListener("keypress", function(e)
-{
-    if(e.key == "Enter")
-    {
+searchBox.addEventListener("keypress", function (e) {
+    if (e.key == "Enter") {
         e.preventDefault();
         checkWeather(searchBox.value);
     }
