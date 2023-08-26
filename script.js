8c5bb4a844fcde3ff56f31f01b449296441e35d0
Enzo Petela <enzo03453@gmail.com>
2023-08-26 20:39:50 -0300

    feat: added a list that shows the previous searched cities with some functionality.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apiKey = "bfd8982d9bd4680fcb4ad78530593ded";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const listContainer = document.getElementById("list-container");
+const deleteAllButton = document.getElementById("delete-all");
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -19,7 +20,7 @@ async function checkWeather(city) {
         document.querySelector(".clock").style.display = "none";
     }
     else {
-        
+
         var data = await response.json();
 
 
@@ -27,19 +28,20 @@ async function checkWeather(city) {
 
         changeWeatherData(data);
         changeWeatherImage(data);
+        checkIfExistInList(data.name, data);
+        outputActualTime(data);
 
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".clock").style.display = "block";
         document.querySelector(".error").style.display = "none";
-
-        outputActualTime(data);
-        addCityToList(data);
     }
+    searchBox.value = "";
 }
 
+//Output the time of the searched city based on the UTC and calculated with de timezone's atribute
 function outputActualTime(data) {
     const output = document.querySelector(".clock output");
-    
+
     if (intervalId) {
         clearInterval(intervalId);
     }
@@ -68,13 +70,26 @@ function changeWeatherImage(data) {
     else if (data.weather[0].main == "Mist") weatherIcon.src = "img/mist.png";
 }
 
-function addCityToList(data)
-{
+//Add a city to the <ul> "list-container" as a <li> inside of it
+function addCityToList(data) {
     let li = document.createElement("li");
 
     li.innerHTML = data.name;
 
     listContainer.appendChild(li);
+
+}
+
+//Check if the city exists in the previous searched cities, if true, the function delete the old one
+function checkIfExistInList(input, data) {
+    const liElements = listContainer.querySelectorAll("li");
+    liElements.forEach(li => {
+        if (li.textContent == input) {
+            li.remove();
+        }
+    })
+
+    addCityToList(data);
 }
 
 //To search with the Enter key
@@ -89,3 +104,13 @@ searchBox.addEventListener("keypress", function (e) {
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 });
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        checkWeather(e.target.textContent);
+    }
+}, false)
+
+deleteAllButton.addEventListener("click", () => {
+    listContainer.innerHTML = "";
+})
